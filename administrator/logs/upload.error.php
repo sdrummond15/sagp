7234f3351cd08d88c97d724dc4ba9876cb4175f5
sdrummond15 <silvio@sdrummond.com.br>
2025-06-16 17:56:19 -0300

    .

---
+++ administrator/logs/upload.error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-06-16 20:52:54 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2025-06-16T20:52:54+00:00	INFO ::1	upload	
